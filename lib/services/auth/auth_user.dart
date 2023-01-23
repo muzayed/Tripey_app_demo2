6113bce25e088119161cd5d33ad8a87115ae46a2
muzayed <m.i.bishal@gmail.com>
2023-01-23 15:41:36 +0600

    added tests

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -2,6 +2,15 @@ import 'package:firebase_auth/firebase_auth.dart' show User;
 import 'package:flutter/foundation.dart';
 
 @immutable
+// class AuthUser {
+  
+//   final bool isEmailVerified;
+
+//   const AuthUser({required this.isEmailVerified,});
+  
+//   factory AuthUser.fromFirebase(User user)=> AuthUser(isEmailVerified: user.emailVerified);
+// }
+
 class AuthUser {
   final String? email;
   final bool isEmailVerified;
